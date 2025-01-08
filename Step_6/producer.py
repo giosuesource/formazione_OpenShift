3f2a2bb4a4e15a78d7bc5766e5623e326083dc42
giosuesource <giosue.manzo@sourcesense.com>
2025-01-08 10:56:22 +0100

    Update producer.py

--- Step_6/producer.py
+++ Step_6/producer.py
@@ -3,4 +3,4 @@ from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers=['my_ip:9092'])
 
 for _ in range(100):
-    producer.flush('foobar', b'Messaggio di prova')
+    producer.send('foobar', b'Messaggio di prova')
