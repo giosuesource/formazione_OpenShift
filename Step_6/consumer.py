d193129d2646e8a165219703bb15aaccbfd18829
giosuesource <giosue.manzo@sourcesense.com>
2025-01-08 12:54:31 +0100

    Update consumer.py

--- Step_6/consumer.py
+++ Step_6/consumer.py
@@ -1,7 +1,7 @@
 from kafka import KafkaConsumer
 
 consumer = KafkaConsumer('foobar', group_id='gruppo-consumer', 
-bootstrap_servers=['my_ip:9092'], auto_offset_reset='earliest') 
+bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest') 
 
 for msg in consumer:
     print(msg)
