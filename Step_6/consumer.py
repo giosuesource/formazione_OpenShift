11d289e08091050e9f7ba4759ba03a0ae9147b7e
giosuesource <giosue.manzo@sourcesense.com>
2025-01-08 10:44:38 +0100

    Update consumer.py

--- Step_6/consumer.py
+++ Step_6/consumer.py
@@ -1,8 +1,8 @@
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer('test', group_id='test-consumer-group', 
-bootstrap_servers=['my_ip:9092'], api_version=(0, 10, 1),
-                     auto_offset_reset='earliest')
+consumer = KafkaConsumer('foobar', group_id='gruppo-consumer', #nome topic e group id
+bootstrap_servers=['my_ip:9092'], #ip e porta di Kafka, versione Api
+                     auto_offset_reset='earliest') 
 print("Consuming messages")
 for msg in consumer:
     print(msg)
