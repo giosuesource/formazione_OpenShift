479da78c8d2ab979e100203a70e258040ea9dc9a
giosuesource <giosue.manzo@sourcesense.com>
2025-01-08 12:54:16 +0100

    Update producer.py

--- Step_6/producer.py
+++ Step_6/producer.py
@@ -1,6 +1,6 @@
 from kafka import KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers=['my_ip:9092'])
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 
 for _ in range(100):
     producer.send('foobar', b'Messaggio di prova')
